4024d9e3f0d03c1bb079cd9b443ded708215c2f4
Ivan Vegner <ivanvegner@gmail.com>
2016-12-05 17:11:54 -0500

    Fix auto-swing issue
    Autoswing was checking for "in a fight!" as its location type, whereas the actual text while in combat is "in combat!".

--- InitiumPro.js
+++ InitiumPro.js
@@ -140,7 +140,7 @@ function loadLocalMerchantDetails() {
 function keepPunching() {
     //for a more CircleMUD feel
     if(AUTO_SWING) {
-        if(loc.type==="in a fight!" && window.urlParams.type==="attack" && player.health>AUTO_FLEE) {
+        if((loc.type==="in a fight!" || loc.type==="in combat!") && window.urlParams.type==="attack" && player.health>AUTO_FLEE) {
             if(window.urlParams.hand==="RightHand")  window.combatAttackWithRightHand();  else  window.combatAttackWithLeftHand();
             combatMessage("Attacking with "+window.urlParams.hand,"AUTO-SWING");
         }
